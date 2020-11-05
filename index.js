4f813ff4c06805c92916e23001e43b121b648082
Arkaprava Chakraborty <63491004+arkachakrabrty@users.noreply.github.com>
2020-11-05 17:04:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const mysql = require('mysql');
 const bodyparser = require('body-parser');
 const app = express();
 
-app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+
 
 const con =  mysql.createConnection({
     host:"localhost",
@@ -18,6 +19,7 @@ app.get('/', function(req,res){
 })
 
 app.post('/addUser',(req,res)=>{
+
     var firstName = req.body.firstName;
     var lastName = req.body.lastName;
     var regNumber = req.body.regNumber;
@@ -37,59 +39,63 @@ app.post('/addUser',(req,res)=>{
                 status:200,
                 message:"Data is entered."
             });
-        };
+        }
     });
+});
+
+app.post("/updateUser",(req, res)=>{
+
+    
+  let firstName = req.body.firstName;
+  let lastName = req.body.lastName;
+  let regNumber = req.body.regNumber;
+  let newFirstName = req.body.newFirstName;
+  let newLastName = req.body.newLastName;
+  let newRegNumber = req.body.newRegNumber;
+  let query = `UPDATE userinfo SET firstName = "${newFirstName}" , lastName = "${newLastName}" , regNumber = ${newRegNumber} WHERE firstName = "${firstName}" AND lastName = "${lastName}" AND regNumber = ${regNumber}`;
+    
+    
+  con.query(query,(err, results) => {
+    if(err) {
+      res.send(err.message);
+      console.log(err.sql);
+      console.log(err.message);
+    } else {
+      console.log("Data is updated.");
+      res.send({
+        status: 200,
+        message: "Data is updated."
+      });
+    }
+  });
+});
+
+app.post("/deleteUser",(req, res)=>{
+
+  let firstName = req.body.firstName;
+  let lastName = req.body.lastName;
+  let regNumber = req.body.regNumber;
+  let query = `DELETE FROM userinfo WHERE firstName = "${firstName}" AND lastName = "${lastName}" AND regNumber = ${regNumber}`;
+    
+    
+  con.query(query,(err, results)=>{
+    if (err) {
+      res.send(err.message);
+      console.log(err.message);
+    } 
+    else {
+      console.log("Data has been deleted.");
+      res.send({
+        status:200,
+        message:"Data is deleted."
+      });
+    }
+
+  });
 
 });
 
-app.post("/updateUser", (req, res) => {
-    
-    let firstName = req.body.firstName;
-    let lastName = req.body.lastName;
-    let regNumber = req.body.regNumber;
-    let newFirstName = req.body.newFirstName;
-    let newLastName = req.body.newLastName;
-    let newRegNumber = req.body.newRegNumber;
-    let query = `UPDATE USERINFO SET firstName = "${newFirstName}" , lastName = "${newLastName}" , regNumber = ${newRegNumber} WHERE firstName = "${firstName}" AND lastName = "${lastName}" AND regNumber = ${regNumber} `;
-    con.query(query, (err, results) => {
-      if (err) {
-        res.send(err.message);
-        console.log(err.sql);
-        console.log(err.message);
-      } else {
-        console.log("Data is updated.");
-        res.send({
-          status: 200,
-          message: "Data is updated.",
-        });
-      }
-    });
-  });
-
-  app.post("/deleteUser", (req, res) => {
-    let firstName = req.body.firstName;
-    let lastName = req.body.lastName;
-    let regNumber = req.body.regNumber;
-    let query = `DELETE FROM USERINFO WHERE firstName = "${firstName}" AND lastName = "${lastName}" AND regNumber = ${regNumber} `;
-    
-    con.query(query, (err, results) => {
-      if (err) {
-        res.send(err.message);
-        console.log(err.message);
-      } 
-      else {
-        console.log("Data has been deleted.");
-        res.send({
-          status: 200,
-          message: "Data is deleted.",
-        });
-      }
-
-    });
-
-  });
-
 const PORT = 6001;
 app.listen(PORT,()=>{
-    console.log('Serving running at ${PORT} ');
+    console.log('Server running!');
 })
